55fd119093dac16a65a41e7ebdd2f89476334257
saya0118 <chemi0802@gmail.com>
2022-04-01 22:41:50 -0700

    created page component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
-import React from 'react';
-import './App.css';
-import {Header} from './components/Header';
+import React from "react";
+import "./App.css";
+import { Header } from "./components/Header";
+import { Page } from "./components/Page";
 
 export const App = () => {
-
   return (
     <div className="App">
-      <Header/>
+      <Header />
+      <Page />
     </div>
-  )
-}
+  );
+};
