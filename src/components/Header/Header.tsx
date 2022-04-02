5c86c9d19258fc64e68fff04f03a6dc5f82ce604
saya0118 <chemi0802@gmail.com>
2022-04-01 22:30:13 -0700

    created header component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,10 +1,23 @@
-import React from 'react';
-import styles from './Header.module.css';
+import React from "react";
+import styles from "./Header.module.css";
 
-export const Header = () =>{
-    return (
-        <div className={styles.header}>
-            
-        </div>
-    )
-}
+export const Header = () => {
+  return (
+    <div className={styles.header}>
+      <div className={styles["header-container"]}>
+        <h1 className={styles["header-title"]}>Voyage Notes</h1>
+        <ul className={styles['header-ul']}>
+          <li className={styles["header-list"]}>
+            <a className={styles["header-link"]}>Home</a>
+          </li>
+          <li className={styles["header-list"]}>
+            <a className={styles["header-link"]}>About</a>
+          </li>
+          <li className={styles["header-list"]}>
+            <a className={styles["header-link"]}>Diary</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
