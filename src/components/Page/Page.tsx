4c659b038597819c2a9b1638e7a6d40739d8ab3a
saya0118 <chemi0802@gmail.com>
2022-04-02 23:35:12 -0700

    created search bar component

--- src/components/Page/Page.tsx
+++ src/components/Page/Page.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import styles from './Page.module.css';
+import { SearchBar } from '../SearchBar';
 
 export const Page = () => {
     return (
         <div className={styles['page']}>
-            
+            <SearchBar/>
         </div>
     )
 }
---
+++ src/components/SearchBar/SearchBar.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './SearchBar.module.css';
+
+export const SearchBar = () => {
+    return (
+        <div className={styles['search-bar']}>
+            
+        </div>
+    )
+}
