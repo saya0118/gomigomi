9b4510da2620209ecb0a9cc467317ffc27144a4b
saya0118 <chemi0802@gmail.com>
2022-04-03 23:22:33 -0700

    created search bar and search button

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -4,7 +4,8 @@ import styles from './SearchBar.module.css';
 export const SearchBar = () => {
     return (
         <div className={styles['search-bar']}>
-            
+            <input className={styles['search-input']} type="text" />
+            <button className={styles['search-button']} type="submit"></button>
         </div>
     )
 }
