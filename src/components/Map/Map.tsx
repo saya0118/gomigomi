db43e3e79d6f6d587e5f0cb9e41a5adee7648dd5
saya0118 <chemi0802@gmail.com>
2022-04-04 23:56:43 -0700

    created map component

---
+++ src/components/Map/Map.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './Map.module.css';
+
+export const Map = () => {
+    return (
+        <div className={styles.map}>
+            
+        </div>
+    )
+}
--- src/components/Page/Page.tsx
+++ src/components/Page/Page.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import styles from "./Page.module.css";
+import { Map } from "../Map";
 import { SearchBox } from "../SearchBox";
 
 export const Page = () => {
   return (
     <div className={styles["page"]}>
+      <Map />
       <SearchBox />
     </div>
   );
