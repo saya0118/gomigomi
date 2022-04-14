343ae2a5647dda1db7ed925232fde7a96da4d66b
saya0118 <chemi0802@gmail.com>
2022-04-13 21:11:51 -0700

    installed eslint and tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  purge: [
+    './src/compoents'
+  ],
+  content: [],
+  theme: {
+    colors:{
+      'white': '#eeeee4',
+      'blue': '#50929d',
+      'indigo': '#11303fde',
+      'brown': '#644434',
+    },
+    extend: {},
+  },
+  plugins: [],
+}
